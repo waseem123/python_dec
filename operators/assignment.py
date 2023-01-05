a1da42b71043666aea47117a7f3b1e3fbfed5afe
WASEEM ATTAR <waseem_6957@yahoo.com>
2023-01-05 11:03:21 +0530

    Operators In Python
    Arithmetic and Assignment Operators

---
+++ operators/assignment.py
@@ -0,0 +1,23 @@
+x = 100
+print(x)
+z = 65
+print(z)
+name = "Akash"
+print(name)
+x=name
+print(x)
+
+x = y = z = 125
+print(x,y,z)
+
+#addition-assignment
+x = 5
+x += 2 #(x=x+2)
+print(x)
+
+# subtraction assignment
+x -= 5
+print(x)
+
+x *= 5
+print(x)
