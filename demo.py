383aaf8b53e32f1b009a1ed1c31d22c7a3b95a41
WASEEM ATTAR <waseem_6957@yahoo.com>
2023-01-06 10:51:17 +0530

    Intro code
    Python Installation and execution

---
+++ demo.py
@@ -0,0 +1,7 @@
+name = "Alex"
+age = 25
+city = "Mumbai"
+
+print("My name is",name)
+print("I am",age,"years old")
+print("I live in",city)
---
+++ operations.py
@@ -0,0 +1,25 @@
+#addition operations
+x = 120
+y = 30
+z = x + y
+print(z)
+
+#subtraction operation
+x = 120
+y = 30
+z = x - y
+print(z)
+
+#multiplication operation
+x = 120
+y = 30
+z = x * y
+print(z)
+
+#division operation
+x = 120
+y = 30
+z = x / y
+print(z)
+
+#To run this code press ctrl+b
