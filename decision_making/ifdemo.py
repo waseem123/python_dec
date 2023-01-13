c8d456bc81069072a07e8336d4132ea0c049d784
WASEEM ATTAR <waseem_6957@yahoo.com>
2023-01-13 11:18:59 +0530

    Decision Making

---
+++ decision_making/ifdemo.py
@@ -0,0 +1,4 @@
+x = 100
+y = int(input("ENTER A NUMBER - "))
+if x>y:
+	print(f"YES {x} is greater than {y}")
---
+++ decision_making/ifelse.py
@@ -0,0 +1,9 @@
+marks = int(input("ENTER YOUR MARKS - "))
+
+if marks<=100:
+	if marks>=40:
+		print("CONGRATULATIONS! YOU ARE PASS.")
+	else:
+		print("SORRY!" "YOU ARE FAIL.")
+else:
+	print("INVALID MARKS")
