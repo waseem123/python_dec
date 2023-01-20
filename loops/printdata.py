688b3fd043a1d1d40366afadcdc167294f4ba4a9
WASEEM ATTAR <waseem_6957@yahoo.com>
2023-01-20 11:41:33 +0530

    Loops
    To perform any task in a repetitive manner
    we use the Loops. Basically programming languages have 2 types of loops 
     1. Entry Controlled
     		The loop looks for the condition to be satisfied before its execution
     		a. While
     		b. for - in
     2. Exit Controlled
     		The loop who looks for the condition to be satisfied after its execution that will decide the next iteration of loop..
     		a. do-while
    
    1. WHILE - 
    	works on condition, and it executes unless and until the condition does not get failed. It is indeterminate.
    
    	Syntax - 
    		while CONDITION:
    			statement 1
    			statement 2
    			---
    			statement N
    
    
    2. FOR IN- 
    	works on ranges. It is a determinate loop. i.e. It is already having an execution plan for start and end.
    
    	Syntax -
    
    		for i in range(START,END):
    			body of the loop
    	Example-
    
    		for i in range(1,5):
    			print(i)
    	In the above example, i will be assigned 1 and prints 1. Then i get assigned value 2 and print 2. Then i get assigned value 3 and print 3. Then i get assigned value 4 and print 4. At last the END - 1 is 4 hence, the loop will get terminated.

---
+++ loops/printdata.py
@@ -0,0 +1,10 @@
+name = "Accept your mistakes, cry and move on"
+print(len(name))
+length = len(name)
+
+i=0
+while i<length:
+	print(f"{i} - {name[i]}")
+	i+=1
+else:
+	print("END OF STATEMENT")
---
+++ loops/rangefunction.py
@@ -0,0 +1,11 @@
+# Range function works on two values 
+# i.e. START and END
+# where the range function considers START
+# as the starting point
+# and END-1 as the end point
+
+x = range(1,5-2)
+10,11,12,13,14,15,16,17,18,19
+print(x)
+
+y = range(-1,0)
