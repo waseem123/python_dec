239399cf4cbe5b92e79007e63f6581e21748edf5
WASEEM ATTAR <waseem_6957@yahoo.com>
2023-01-23 10:36:24 +0530

    Add files via upload

---
+++ loops/sumofnumbers.py
@@ -0,0 +1,9 @@
+num = int(input("ENTER A NUMBER -"))
+
+i = 1
+result = 0
+while i<=num:
+	result += i #result = result + i
+	i += 1
+
+print(result)
---
+++ loops/sumofnumbers_forloop.py
@@ -0,0 +1,6 @@
+num = 5
+result = 0
+for i in range(1,num+1):
+	result+=i
+
+print(result)
